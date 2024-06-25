ed8c8e324558769644becf187b11da15dd7d549e
Mohammed El Saeed <67608414+moSaeed15@users.noreply.github.com>
2024-06-25 09:19:45 +0300

    set up sentry

--- src/components/RecentProjects.tsx
+++ src/components/RecentProjects.tsx
@@ -1,5 +1,6 @@
 import { projects } from '@/data';
 import React from 'react';
+import Card from './Card';
 
 const RecentProjects = () => {
   return (
@@ -8,13 +9,13 @@ const RecentProjects = () => {
         A small selection of{' '}
         <span className="text-purple"> recent projects</span>
       </h1>
-      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+      <div className="flex flex-wrap items-center justify-center p-4 gap-x-44 gap-y-16 mt-10">
         {projects.map(({ id, title, des, img, iconLists, link }) => (
           <div
             key={id}
             className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
           >
-            {title}
+            <Card />
           </div>
         ))}
       </div>
