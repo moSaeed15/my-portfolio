e3e7ab7c17f9812bca8679fc71f6b17442844fe1
Mohammed El Saeed <67608414+moSaeed15@users.noreply.github.com>
2024-07-06 13:58:02 +0300

    edited navbar size

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,10 @@
 const NavBar = () => {
   return (
     <div className="flex relative top-0 sm:mx-auto  px-0 sm:px-5 bg-transparent  justify-between py-5 items-center z-40">
-      <h1 className=" text-sm  sm:text-xl font-bold uppercase ">
+      <h1 className=" text-base  sm:text-xl font-bold uppercase ">
         Mohammed El Saeed
       </h1>
-      <ul className="flex justify-between items-center gap-1 sm:gap-3 sm:text-base text-xs ">
+      <ul className="flex justify-between items-center gap-2 sm:gap-3 sm:text-base text-sm ">
         <li className="hover:opacity-50 duration-150 transition-opacity">
           <a href="#projects"> My Projects </a>
         </li>
