2a541047ec0349f2a759eea4af2a0fb101c5670f
Mohammed El Saeed <67608414+moSaeed15@users.noreply.github.com>
2024-07-23 14:15:48 +0300

    removed bug

--- tests/externalLink-test.spec.ts
+++ tests/externalLink-test.spec.ts
@@ -5,8 +5,6 @@ test('Github redirect works as expected', async ({ page }) => {
 
   await page.goto('https://github.com/moSaeed15/cogniverse');
 
-  await page.getByLabel('commits by moSaeed15').click();
-
   expect(page.url()).toBe('https://github.com/moSaeed15/cogniverse');
 
   await page.goBack();
